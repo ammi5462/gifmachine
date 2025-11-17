255991a90bd9fd86af58558a2a06f533e8b5fa07
ammi5462 <ammyahsanchaudhary@gmail.com>
2025-11-17 15:37:20 -0500

    Update deploy.sh

--- deploy-scripts/deploy.sh
+++ deploy-scripts/deploy.sh
@@ -1,44 +1,30 @@
 #!/bin/bash
 set -e
 
-# Variables
-ECR_ACCOUNT="156916772993"
-REGION="us-east-1"
-IMAGE_NAME="gifmachine"
-CONTAINER_NAME="gifmachine"
-DB_URL="postgres://gifadmin:mv62jCJZeJXZSzeS4OMe@gifmachine.c2pccckqsbed.us-east-1.rds.amazonaws.com:5432/gifmachine?sslmode=require"
-GIFMACHINE_PASSWORD="mySuperSecret"
-
 # Login to ECR
-echo "Logging in to ECR..."
-aws ecr get-login-password --region $REGION | docker login --username AWS --password-stdin $ECR_ACCOUNT.dkr.ecr.$REGION.amazonaws.com
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 156916772993.dkr.ecr.us-east-1.amazonaws.com
 
-# Pull latest image from ECR
-echo "Pulling latest Docker image..."
-docker pull $ECR_ACCOUNT.dkr.ecr.$REGION.amazonaws.com/$IMAGE_NAME:latest
+# Pull latest image
+docker pull 156916772993.dkr.ecr.us-east-1.amazonaws.com/gifmachine:latest
 
 # Stop and remove old container
-echo "Stopping and removing old container if it exists..."
-docker stop $CONTAINER_NAME || true
-docker rm $CONTAINER_NAME || true
+docker stop gifmachine || true
+docker rm gifmachine || true
 
-# Remove dangling/unused images to free space
-echo "Cleaning up unused Docker images..."
-docker image prune -af
-
-# Run new container
-echo "Starting new container..."
+# Run new container with consistent DB variables
 docker run -d \
-  --name $CONTAINER_NAME \
+  --name gifmachine \
   --restart unless-stopped \
   -p 80:4567 \
-  -e DATABASE_URL="$DB_URL" \
-  -e GIFMACHINE_PASSWORD="$GIFMACHINE_PASSWORD" \
-  $ECR_ACCOUNT.dkr.ecr.$REGION.amazonaws.com/$IMAGE_NAME:latest
+  -e DB_USER=gifadmin \
+  -e DB_PASSWORD="mv62jCJZeJXZSzeS4OMe" \
+  -e DB_HOST=gifmachine.c2pccckqsbed.us-east-1.rds.amazonaws.com \
+  -e DB_PORT=5432 \
+  -e DB_NAME=gifmachine \
+  -e GIFMACHINE_PASSWORD=mySuperSecret \
+  156916772993.dkr.ecr.us-east-1.amazonaws.com/gifmachine:latest
 
-# Validate service after 30 seconds
-echo "Waiting 30 seconds to validate service..."
+# Wait and validate
 sleep 30
-curl -f http://localhost || { echo "Validation failed!"; exit 1; }
-
+curl -f http://localhost || exit 1
 echo "GIFMachine is up and running!"
